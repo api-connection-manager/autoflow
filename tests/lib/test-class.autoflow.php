d5e3c414678d344ac6fd9456e2b357d6450ae92b
daithi-coombes <webeire@gmail.com>
2013-08-31 12:09:22 +0100

    autoflow test case

---
+++ tests/lib/test-class.autoflow.php
@@ -0,0 +1,14 @@
+<?php
+
+class AutoflowTest extends WP_UnitTestCase{
+
+	protected $obj;
+
+	function setUp(){
+		//$this->obj = new Autoflow();
+	}
+
+	function test_foo(){
+		$this->assertTrue(true);
+	}
+}
